dcceac1780d7eb2ad3a584c054994785bb3212bf
Andres Osorio <andres.osoriogr@gmail.com>
2024-09-10 20:30:21 -0500

    fix:  warning - closes #7

--- lib/aurora_web/gettext.ex
+++ lib/aurora_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule AuroraWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :aurora
+  use Gettext.Backend, otp_app: :aurora
 end
